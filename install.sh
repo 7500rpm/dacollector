a61f23c3ef985fa217f1a082e17770410ad7b72a
7500rpm <mikolash65@gmail.com>
2024-05-09 18:25:51 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,10 @@ apt-get install curl -y
 apt-get install net-tools -y
 apt-get install exiftool -y
 apt-get install tcpdump -y
+
+sudo chmod +x conf.sh
+sudo chmod +x cpu.sh
+sudo chmod +x log.sh
+sudo chmod +x meta.sh
+sudo chmod +x network.sh
+sudo chmod +x virustotal.sh
