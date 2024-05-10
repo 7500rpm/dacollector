d80b9317229c162d423cc473c87fec29ee5b2b08
7500rpm <mikolash65@gmail.com>
2024-05-10 17:14:14 +0300

    Update log.sh

--- log.sh
+++ log.sh
@@ -4,18 +4,30 @@
 log_directory="/var/log"
 
 # Путь к файлу для записи найденных строк
-output_file="/home/ilya/Desktop/error_logs.txt"
+output_file1="/home/ilya/Desktop/auth_log.txt"
+output_file2="/home/ilya/Desktop/error_logs.txt"
+
 
 # Очистим файл перед началом сканирования
-> "$output_file"
+> "$output_file1"
+> "$output_file2"
 
 # Проходим по файлам auth.log и syslog
-for logfile in "$log_directory/auth.log" "$log_directory/syslog"; do
+for logfile in "$log_directory/auth.log"; do
     # Проверяем, существует ли файл
     if [[ -f "$logfile" ]]; then
         # Ищем строки с ключевыми словами и записываем их в файл
-        grep -Ei "error|failed|denied" "$logfile" >> "$output_file"
+        grep -Ei "error|failed|denied" "$logfile" >> "$output_file1"
     fi
 done
 
-echo "Logs with keywords 'error', 'failed', 'denied', '/etc/passwd' have been saved to $output_file"
+# Проходим по файлам auth.log и syslog
+for logfile in "$log_directory/syslog"; do
+    # Проверяем, существует ли файл
+    if [[ -f "$logfile" ]]; then
+        # Ищем строки с ключевыми словами и записываем их в файл
+        grep -Ei "error|failed|denied" "$logfile" >> "$output_file2"
+    fi
+done
+
+echo "Logs with keywords have been saved to $output_file1 and $output_file2"
