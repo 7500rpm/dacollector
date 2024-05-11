0353003f2a5b142c84a214d6ed58c499ef6a759e
7500rpm <mikolash65@gmail.com>
2024-05-11 16:04:16 +0300

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -8,11 +8,8 @@ output_file="/home/ilya/Desktop/cpu_load.log"
 
 # Записываем 10 записей о загрузке процессора
 for ((i=1; i<=10; i++)); do
-    # Получаем данные о загрузке процессора за последние 10 секунд
-    cpu_load=$(top -n 1 -b | awk 'NR > 7 { sum += $9; } END { print sum; }')
-
-    # Записываем информацию в файл
-    echo "$(date +"%Y-%m-%d %H:%M:%S") CPU load: $cpu_load" >> "$output_file"
+    echo "$(date +"%Y-%m-%d %H:%M:%S")" >> "$output_file"
+    top -n 1 -b | awk '/^%Cpu/{print $0}' >> "$output_file"
 
     # Ждем 1 секунду перед следующей записью
     sleep 1
