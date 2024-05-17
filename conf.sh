7462f88f12968320f5bb71964d3d3a6fac88afff
7500rpm <mikolash65@gmail.com>
2024-05-17 16:57:45 +0300

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -3,20 +3,25 @@
 # Путь к директории с конфигурационными файлами
 config_dir="/etc"
 
-# Получаем текущую дату и время
+# Получаем текущую дату и время в формате Unix timestamp
 current_date=$(date +%s)
 
-# Находим дату, которая была месяц назад
+# Находим дату, которая была месяц назад в формате Unix timestamp
 one_month_ago=$(date -d '1 month ago' +%s)
-
-# Получаем список конфигурационных файлов с расширением .conf и с датой изменения
-config_files=$(find $config_dir -type f -name '*.conf' -newermt "$(date -d '1 month ago' '+%Y-%m-%d %H:%M:%S')")
 
 # Выводим заголовок
 echo "Последние изменения конфигурационных файлов за последний месяц в директории $config_dir:"
 
-# Проходим по каждому файлу и выводим время последнего изменения
-for file in $config_files; do
-    last_modified=$(stat -c %y "$file")  # Получаем время последнего изменения файла
-    echo "$file: $last_modified"
+# Проходим по каждому файлу с расширением .conf в директории /etc и проверяем его время изменения с помощью exiftool
+for file in "$config_dir"/*.conf; do
+    # Получаем время последнего изменения файла с помощью exiftool
+    last_modified=$(exiftool -FileModifyDate -d '%Y-%m-%d %H:%M:%S' -s3 "$file")
+
+    # Преобразуем время последнего изменения в формат Unix timestamp
+    last_modified_timestamp=$(date -d "$last_modified" +%s)
+
+    # Проверяем, был ли файл изменен в течение последнего месяца
+    if ((last_modified_timestamp >= one_month_ago && last_modified_timestamp <= current_date)); then
+        echo "$file: $last_modified"
+    fi
 done
